f5de7576d586bc88ee37b499c06fbba6de484b7a
Klever-Sobrinho <kleverfortaleza@gmail.com>
2019-02-11 16:47:23 -0300

    Learning typescript

---
+++ hello-typescript/typetest.ts
@@ -0,0 +1,27 @@
+var myVar = 'my var!';
+
+function myFunc(x, y) {
+    return x + y;
+}
+
+// ES6 or ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numbers = [ 1, 2, 3 ];
+numbers.map(function(value) {
+    return value * 2;
+});
+
+numbers.map(value => value *2); // ES 2015
+
+class Mathematics {
+    soma( x, y ){
+        return x + y;
+    }
+}
+
+var n1: any = 'testString';
+n1 = 12;
+
+// tsc typetest.ts Compile Typescript
